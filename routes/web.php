aef87f9bf41babe4ffb35bc7f9a14d38fc6ce688
Moehtet-hlaing <seoyuzuki#2310Mon>
2025-05-19 09:27:34 +0630

    add database file and run migration

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\NinjaController;
+use App\Http\Controllers\Product\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/ninjas", [NinjaController::class, "index"])->name('ninjas.index');
+Route::get("/ninjas/create", [NinjaController::class, "create"])->name('ninjas.create');
+Route::get("/ninjas/{ninja}", [NinjaController::class, "show"])->name('ninjas.show');
+Route::post("/ninjas",[NinjaController::class, "store"])->name('ninjas.store');
+Route::delete("/ninjas/{ninja}", [NinjaController::class, "destroy"])->name('ninjas.destroy');
