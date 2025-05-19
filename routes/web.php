d7e3ca7bb3962f3e039dc137b0d85dab37c8de15
Moehtet-hlaing <seoyuzuki#2310Mon>
2025-05-19 09:52:28 +0630

    add routes for register and login page

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NinjaController;
-use App\Http\Controllers\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register',[AuthController::class, "showRegister"])->name('show.register');
+Route::get('/login',[AuthController::class, "showLogin"])->name('show.login');
 
 Route::get("/ninjas", [NinjaController::class, "index"])->name('ninjas.index');
 Route::get("/ninjas/create", [NinjaController::class, "create"])->name('ninjas.create');
