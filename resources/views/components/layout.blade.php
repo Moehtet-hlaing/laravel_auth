05a9ac87ffd9b46db6f837b4e24938d894db28d3
Moehtet-hlaing <seoyuzuki#2310Mon>
2025-05-19 16:44:28 +0630

    make changes in navbar,authenticated or not

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,27 +8,39 @@
     <title>Ninja Network</title>
     @vite('resources/css/app.css')
 </head>
+
 <body>
-    @if(session('success'))
-    <div class="p-4 text-center bg-green-50 text-green-500 font-bold">
-        {{ session('success') }}
-    </div>
+    @if (session('success'))
+        <div class="p-4 text-center bg-green-50 text-green-500 font-bold">
+            {{ session('success') }}
+        </div>
     @endif
     <header>
         <nav>
-            <h1>Ninja Network</h1>
-            <a href="{{ route('ninjas.index')}} ">All Ninjas</a>
-            <a href="{{ route('ninjas.create')}} ">Create New Ninja</a>
-            <a href="{{ route('show.login')}} ">Login</a>
-            <a href="{{ route('show.register')}} ">Register</a>
-            <form action="{{ route('logout') }}" method="POST" class="m-0">
-                @csrf
-                <button class="btn">logout</button>
-            </form>
+            <h1>
+                <a href="{{ route('ninjas.index') }} ">Ninja Network</a>
+            </h1>
+            @guest
+                <a href="{{ route('show.login') }} ">Login</a>
+                <a href="{{ route('show.register') }} ">Register</a>
+            @endguest
+
+            @auth
+            <span class="p-2 border-r-2">
+                Hi there, {{ Auth::user()->name }}
+            </span>
+            <a href="{{ route('ninjas.create') }} ">Create New Ninja</a>
+                <form action="{{ route('logout') }}" method="POST" class="m-0">
+                    @csrf
+                    <button class="btn">logout</button>
+                </form>
+            @endauth
+
         </nav>
     </header>
     <main class="container">
         {{ $slot }}
     </main>
 </body>
+
 </html>
